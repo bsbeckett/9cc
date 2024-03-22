5b6d48fd0c6af59c0f336f30bddcf66be33fd6a4
Brad Beckett <brad@becketthome.com>
2024-03-22 11:37:50 -0400

    Added a basic parser and support for *, /, ()

--- 9cc.c
+++ 9cc.c
@@ -40,12 +40,16 @@ void error_at(char *loc, char *fmt, ...) {
 
   int pos = loc - user_input;
   fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "%*s", pos, "");
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
 }
+
+// /////////////////////////////////////
+// Tokenizer
+// /////////////////////////////////////
 
 bool consume(char op) {
 	if (token->kind != TK_RESERVED || token->str[0] != op) 
@@ -95,7 +99,8 @@ Token *tokenize(char *p) {
 		}
 
 		// Operators
-		if (*p == '+' || *p == '-') {
+		if  (*p == '+' || *p == '-' || *p == '*' || *p == '/' ||
+             *p == '(' || *p == ')') {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -114,33 +119,137 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+// ///////////////////////////////////////
+// Parser
+// ///////////////////////////////////////
+
+// Types of AST nodes
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // Numeric literal
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind;	// Node type
+	Node *lhs;		// Left side
+	Node *rhs;		// Right side
+	int val;		// Used only when kind is ND_NUM
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+Node *primary() {
+	// If the next token is "(", it should be "(" expr ")"
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	// Otherwise it should be a number
+	return new_node_num(expect_number());
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+// ////////////////////////////////
+// Code Generator
+// ////////////////////////////////
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
+}
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
 		return 1;
 	}
 
+	// Tokenize and parse the input
 	user_input = argv[1];
-	token = tokenize(argv[1]);
+	token = tokenize(user_input);
+	Node *node = expr();	
 
+	// Output the first part of the assembly
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 
-	// First token must be a number
-	printf("  mov rax, %d\n", expect_number());
+	// Generate code while descending the AST
+	gen(node);
 
-	// ... followed by either '+ <number>' or '- <number>'.
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
+	// The value of the entire expression should be at the top of the stack.
+	// So, load it into RAX and use it as the return value from the function.
+	printf("  pop rax\n");
 	printf("  ret\n");
 	return 0;
 }
